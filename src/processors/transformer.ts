a2bb2c14cf07cf21d6eb27d0cb6fb54fb0c3d31d
Nzanzu Lwanzo <160411106+Nzanzu-Lwanzo@users.noreply.github.com>
2025-07-11 14:20:05 +0200

    remove unused variable

--- src/processors/transformer.ts
+++ src/processors/transformer.ts
@@ -1,7 +1,5 @@
 import { Transform } from "node:stream";
 
-let chucks: any[] = [];
-
 const streamLogger = new Transform({
   transform(chunk, encoding, callback) {
     console.log(`>> STREAM PASSED THROUGH THE TRANSFORMER`);
